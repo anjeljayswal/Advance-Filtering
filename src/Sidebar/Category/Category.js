96ad67a7d077554de2f76606741383b0e040e7a0
anjeljayswal <anjeljayswal@gmail.com>
2023-07-20 17:52:39 +0545

    Category page

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,8 +1,32 @@
-
+import './Category.css'
 function Category() {
   return (
     <div>
-      Category
+      <h2 className="sidebar-title">Category</h2>
+
+      <div>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sneaker
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+        <label htmlFor="" className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Heels
+        </label>
+        
+      </div>
     </div>
   )
 }
